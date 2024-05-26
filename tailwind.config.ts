987633277d71f7cacaddd7ae08c557b3236edc9a
dylann97 <d.xavero@hotmail.com>
2024-05-26 11:26:35 +0200

    🚧 F01: Tailwind Config > Add a custom color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,9 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        "red-light": "#F35C7A",
+      },
     },
   },
   plugins: [],
