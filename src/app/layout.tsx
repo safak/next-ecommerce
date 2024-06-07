7813b78d06e828d81303422326e785d375792d4f
AYMEN GHERDAINE <gherdaineaymen1995@gmail.com>
2024-06-07 16:15:34 -0400

    setup the layout and create navbar and footer components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
---
+++ src/app/login/page.tsx
@@ -0,0 +1,5 @@
+const LoginPage = () => {
+  return <div>Login Page</div>;
+};
+
+export default LoginPage;
