e6067a79991978f6e403578d482eb7140dd0ee32
Miled <midou.slm8@gmail.com>
2024-09-01 19:41:01 +0100

    refact folders order

---
+++ src/app/(user)/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "@/app/globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import { WixClientContextProvider } from "@/context/wixContext";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Leo Display",
+  description: "an e-comerce site",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Navbar/>
+        {children}
+        <Footer/>
+        </body>
+    </html>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,18 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
-import { WixClientContextProvider } from "@/context/wixContext";
 
-const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "Leo Display",
-  description: "an e-comerce site",
-};
+const poppins = Poppins({
+    weight: ["400", "500", "600", "700"],
+    subsets: ["latin"],
+  });
+  
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <Navbar/>
-        {children}
-        <Footer/>
-        </body>
+    <html lang="en" className={`${poppins.className}`}>
+      <body>{children}</body>
     </html>
   );
 }
